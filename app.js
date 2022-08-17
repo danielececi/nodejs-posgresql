bb633080b8e4f6974626494a3c7d7df59b18b995
danielececi <danielececi86@gmail.com>
2022-08-17 14:11:15 +0200

    aggiornato app.js con contenuto helloworld.js

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
 
-var app = express();
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/quotes', quotesRouter);
-
-module.exports = app;
